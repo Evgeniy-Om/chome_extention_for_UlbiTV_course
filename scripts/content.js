1491b1a68ec9ec58facdfb7f2855b3cbf5c8d9f6
Evgeniy-Om <evgenicuss@gmail.com>
2022-11-06 20:49:50 +0300

    version 1.1

---
+++ scripts/content.js
@@ -0,0 +1,76 @@
+let timeoutID
+const showDisplaySpeed = (currentSpeedIndex) => {
+    let prevDisplaySpeed = document.querySelector('.displaySpeed')
+    if (prevDisplaySpeed) document.body.removeChild(prevDisplaySpeed)
+
+    const displaySpeed = document.createElement('div')
+    displaySpeed.classList.add('displaySpeed')
+    displaySpeed.textContent = `Скорость ${currentSpeedIndex*0.25+0.5}x`
+    document.body.appendChild(displaySpeed)
+
+    clearTimeout(timeoutID)
+    timeoutID = setTimeout(() => {
+        let prevDisplaySpeed = document.querySelector('.displaySpeed')
+        if (prevDisplaySpeed) document.body.removeChild(prevDisplaySpeed)
+    },2000)
+
+
+}
+
+if (window.location.host === 'ulbitv.ru') {
+    let player
+
+    new Promise((resolve)=> {
+        const idInterval = window.setInterval(() => {
+            player = document.querySelector(`[data-place="new-player"]`)
+
+            if (player) {
+                clearInterval(idInterval)
+                resolve()
+            }
+        },500)
+
+    }).then(()=> {
+        const iframe = document.getElementsByTagName('iframe')[0]
+        const btn = document.createElement('button')
+        btn.classList.add('btn-player')
+        btn.textContent = "Активировать хоткеи"
+        btn.onclick = () => window.open(iframe.attributes[1].nodeValue)
+        player.after(btn)
+    })
+}
+
+if (window.location.host === 'player-ua.gceuproxy.com') {
+    let play
+
+    new Promise((resolve)=> {
+        const idInterval = window.setInterval(() => {
+            play = document.querySelector(`[data-plyr="play"]`)
+
+            if (play) {
+                clearInterval(idInterval)
+                resolve()
+            }
+        },500)
+
+    }).then(()=> {
+        const speeds = document.querySelectorAll(`[data-plyr="speed"]`)
+        let currentSpeedIndex = Array.from(speeds).findIndex((value) => value.ariaChecked === 'true') ?? 2
+        window.addEventListener('keydown', function(event) {
+            if (event.code === "Space") {
+                play.click()
+            }
+
+            if ((event.key === "+" || event.key === "=") && currentSpeedIndex < 6) {
+                speeds[++currentSpeedIndex].click()
+                showDisplaySpeed(currentSpeedIndex)
+
+            }
+
+            if ((event.key === "-" || event.key === "_") && currentSpeedIndex > 0) {
+                speeds[--currentSpeedIndex].click()
+                showDisplaySpeed(currentSpeedIndex)
+            }
+        })
+    })
+}
